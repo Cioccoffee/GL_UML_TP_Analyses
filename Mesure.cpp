7b480a09305ebf6fefd340d7385f1c1a68651686
Cioccoffee <gallelucie@gmail.com>
2018-05-15 10:39:48 +0200

    Mesure

---
+++ Mesure.cpp
@@ -0,0 +1,64 @@
+//---------- Réalisation de la classe <Mesure> (fichier Mesure.cpp) ------------
+//---------------------------------------------------------------- INCLUDE
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Mesure.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+//
+
+Mesure(string nom, string type, string valeur)
+{
+	this.nom = nom;
+	this.type = type;
+	this.valeur = valeur;
+}
+
+Mesure(const Mesure &uneMesure)
+{
+
+}
+
+bool estSimilaire(Empreinte &maladie)
+{
+
+}
+
+string toString()
+{
+
+}
+
+
+string Nom()
+{
+
+}
+
+
+string Type(){
+
+}
+
+
+string Valeur()
+{
+
+}
+
+
+virtual ~Mesure()
+{
+
+}
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
